1b1be19f46cc962541fc1c04d9fefd265247f2fa
Calvin <calvin@nathandigital.com>
2024-11-18 13:08:20 +0300

    add authentication routes for signup, login, password reset, and logout functionalities

---
+++ Backend/src/routes/auth.routes.js
@@ -0,0 +1,14 @@
+const router = require('express');
+const verifyToken = require('../middlewares/verifyToken');
+const { authController } = require('../controllers');
+
+const router = express.Router();
+
+
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+router.post('/forgot-password', authController.forgotPassword);
+router.post('/reset-password/:token', authController.resetPassword);
+router.post('/logout', verifyToken, authController.logout);
+
+module.exports = router;
