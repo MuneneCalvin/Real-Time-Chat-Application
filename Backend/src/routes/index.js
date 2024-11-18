deb95ef6b76921769d1ddb6a8ffa392fd7c6d85d
Calvin <calvin@nathandigital.com>
2024-11-18 13:09:45 +0300

    add main routing file to integrate authentication routes

---
+++ Backend/src/routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const authRoutes = require('./auth.routes');
+
+const router = express.Router();
+
+const defaultRoutes = [
+    { path: '/auth', route: authRoutes },
+];
+
+defaultRoutes.forEach((route) => {
+    router.use(route.path, route.route);
+});
+
+
+module.exports = router;
