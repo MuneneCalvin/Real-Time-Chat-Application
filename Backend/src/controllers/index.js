753be6749279f5ab78a8ca936e78d407692c52dd
Calvin <calvin@nathandigital.com>
2024-11-28 14:31:05 +0300

    refactor: remove user-related controllers, routes, and services to simplify the codebase

--- Backend/src/controllers/index.js
+++
@@ -1,3 +0,0 @@
-module.exports.authController = require('./auth.controller');
-module.exports.messagesController = require('./messages.controller');
-module.exports.usersController = require('./user.controller');
